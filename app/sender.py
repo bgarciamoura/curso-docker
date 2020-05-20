67baf8dfceffba7c806afd0ae4db8609ec88017d
bgarciamoura <bgarciamoura@gmail.com>
2020-05-20 20:33:06 -0300

    Correção do erro 500

--- app/sender.py
+++ app/sender.py
@@ -1,7 +1,7 @@
 import psycopg2
 from bottle import route, run, request
 
-DSN = 'dbname=email_sender user=postgres host=db'
+DSN = 'dbname=email_sender user=postgres password=postgres host=db'
 SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
 
 def register_message(assunto, mensagem):
